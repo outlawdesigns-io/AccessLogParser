7a1d9b5377139ae2a561bf4b11c50df405940d90
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-07 10:01:14 -0600

    connect to db

--- Request.php
+++ Request.php
@@ -1,10 +1,12 @@
 <?php
 
-class Request{
+require_once __DIR__ . '/Record/Record.php';
 
-    const DRIVER = 'mssql';
-    const DB = 'Sandbox';
-    const TABLE = 'tbl_web_access';
+class Request extends Record{
+
+    const DB = 'web_access';
+    const TABLE = 'requests';
+    const PRIMARYKEY = 'id';
 
     public $id;
     public $host;
@@ -21,16 +23,23 @@ class Request{
 
     public function __construct($id = null)
     {
-        //parent::__construct(self::DRIVER,self::DB,self::TABLE,$id);
+        parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$id);
     }
-    public static function lastRequest(){
+    public static function lastRequest($host){
+        $data = null;
         $results = $GLOBALS['db']
-            ->driver(self::DRIVER)
             ->database(self::DB)
             ->table(self::TABLE)
-            ->select("id")
-            ->orderBy("id desc")
-            ->get("value");
-        return new self($results);
+            ->select(self::PRIMARYKEY)
+            ->where("host","=","'" . $host . "'")
+            ->orderBy(self::PRIMARYKEY . " desc limit 1")
+            ->get();
+        if(!mysqli_num_rows($results)){
+          throw new \Exception('No requests for host: ' . $host);
+        }
+        while($row = mysqli_fetch_assoc($results)){
+          $data = new self($row[self::PRIMARYKEY]);
+        }
+        return $data;
     }
 }
