fa1f85c484b96f0cb56f756bf8518af6280057fd
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-27 11:42:12 -0600

    Extending Message client

--- processes/AttackMonitor.php
+++ processes/AttackMonitor.php
@@ -1,22 +1,34 @@
 <?php
 
 require __DIR__ . '/../models/Request.php';
+require __DIR__ . '/../MessageClient/MessageClient.php';
 
-class AttackMonitor{
+class AttackMonitor extends MessageClient{
 
   const CONFINT = 1.64;
+  const MSGNAME = '404_attack';
 
   protected $_hosts = array();
   protected $_counts = array();
+  protected $_username;
+  protected $_password;
+  protected $_authToken;
 
-  public function __construct(){
-    $this->_get404s();
-    $fatality = $this->_stdv($this->_counts) * self::CONFINT;
-    for($i = 0; $i < count($this->_hosts); $i++){
-      if($this->_counts[$i] >= $fatality){
-        echo $this->_hosts[$i] . " -> " . $this->_counts[$i] . "\n";
-      }
+  public function __construct($username,$password){
+    $this->_username = $username;
+    $this->_password = $password;
+    $this->_authenticate()
+         ->_get404s()
+         ->_parse();
+  }
+  protected function _authenticate(){
+    try{
+      $data = self::authenticate($this->_username,$this->_password);
+      $this->_authToken = $data->token;  
+    }catch(\Exception $e){
+      throw new \Exception($e->getMessage());
     }
+    return $this;
   }
   protected function _get404s(){
     $results = $GLOBALS['db']
@@ -31,6 +43,16 @@ class AttackMonitor{
     while($row = mysqli_fetch_assoc($results)){
       $this->_hosts[] = $row['ip_address'];
       $this->_counts[] = $row['count'];
+    }
+    return $this;
+  }
+  protected function _parse(){
+    $fatality = $this->_stdv($this->_counts) * self::CONFINT;
+    for($i = 0; $i < count($this->_hosts); $i++){
+      if($this->_counts[$i] >= $fatality && !self::isSent(self::MSGNAME,$this->_hosts[$i],$this->_authToken)){
+        echo $this->_hosts[$i] . " -> " . $this->_counts[$i] . "\n";
+        //todo something
+      }
     }
     return $this;
   }
