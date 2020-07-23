8320601f5f272f6924b830b9d25b1efec9adfba2
outlawstar4761@gmail.com <outlawstar4761@gmail.com>
2020-07-23 15:34:16 -0500

    typo

--- Jobs/StartAttackMonitor.php
+++ Jobs/StartAttackMonitor.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../Processes/AttackMonitor.php';
 require_once __DIR__ . '/AccountCredentials.php';
 
 try{
-  $authToken = AttackMonitor::authenticate($username,$password)->token;
+  $authToken = AttackMonitor::authenticate(ACCOUNT_USER,ACCOUNT_PASSWORD)->token;
   $monitor = new AttackMonitor($msgTo,$authToken);
 }catch(\Exception $e){
   echo $e->getMessage() . "\n";
